ff4f6ac5bc97ec249bf64b2a5135ce0352b28328
Jazib Sawar <jazibsawar@gmail.com>
2019-07-26 22:17:56 +0300

    Preview compatibility added

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -3,7 +3,7 @@ import { processObject } from './normalize'
 import { capitalize } from 'lodash'
 
 exports.sourceNodes = async (
-  { actions, createContentDigest },
+  { actions, webhookBody, createContentDigest, getNode },
   {
     apiURL = 'https://api.cosmicjs.com/v1',
     bucketSlug = '',
@@ -11,8 +11,39 @@ exports.sourceNodes = async (
     apiAccess = {},
   }
 ) => {
-  const { createNode } = actions
+  const { createNode, deleteNode } = actions
 
+  /*
+   * Gatsby preview code path!
+   */
+  if (webhookBody && webhookBody.type) {
+    const item = webhookBody.data
+    switch (webhookBody.type) {
+      case 'object.deleted':
+        deleteNode({
+          node: getNode(item._id),
+        })
+        break
+      case 'object.created.draft':
+      case 'object.created.published':
+      case 'object.edited.draft':
+      case 'object.edited.published':
+        const node = processObject(
+          capitalize(item.type_slug),
+          item,
+          createContentDigest
+        )
+        createNode(node)
+        break
+      default:
+        break
+    }
+    return
+  }
+
+  /*
+   * The existing, non-preview code path!
+   */
   const promises = objectTypes.map(objectType =>
     fetchData({
       apiURL,
