6bdcb96814bfddb9295b748d7f6dd8777b4ddaa5
Donovan King <donovan@bloom.market>
2019-08-13 12:10:25 -0700

    Adding ability to feetch on object types with more than 1000 objects

--- src/fetch.js
+++ src/fetch.js
@@ -1,28 +1,69 @@
 import axios from 'axios'
-import { isObject, startsWith, forEach } from 'lodash'
+import { isObject, startsWith, forEach, concat } from 'lodash'
 
 module.exports = async ({ apiURL, bucketSlug, objectType, apiAccess }) => {
-  console.time('Fetch Cosmic JS data')
+  const timeLabel = `Fetch Cosmic JS data for (${objectType})`
+  console.time(timeLabel)
   console.log(`Starting to fetch data from Cosmic JS (${objectType})`)
-
+  let objects = []
+  const limit = 1000
+  let skip = 0
   // Define API endpoint.
   let apiEndpoint = `${apiURL}/${bucketSlug}/objects?type=${objectType}`
 
   if (apiAccess.hasOwnProperty('read_key') && apiAccess.read_key.length !== 0) {
     apiEndpoint = apiEndpoint + `&read_key=${apiAccess.read_key}`
+    apiEndpoint = apiEndpoint + `&limit=${limit}`
   }
-  // Make API request.
+
+  // Make initial API request.
   const documents = await axios(apiEndpoint)
 
-  // Query all data from endpoint
-  console.timeEnd('Fetch Cosmic JS data')
+  // Check for empty object type
+  if (documents.data.objects === undefined) {
+    console.error(`${objectType} error: ${documents.message}`)
+    console.timeEnd(timeLabel)
+    return objects
+  }
+
+  if (documents.data.objects) {
+    objects = documents.data.objects
+  }
+
+  // check if there's more that request limit of objects for object type
+  if (documents.data.total && documents.data.total > limit) {
+    // Query all data from endpoint
+    // calculate number of calls to retrieve entire object type
+    const additionalCallsRequired = Math.ceil(documents.data.total / limit) - 1
+
+    for (let i = 0; i < additionalCallsRequired; i += 1) {
+      // skip previously requested objects
+      skip = skip + limit
+      let skipEndpoint = apiEndpoint + `&skip=${skip}`
+      // Query next batch from endpoint
+      let response = await axios(skipEndpoint)
+      if (response.data.objects) {
+        objects = concat(objects, response.data.objects)
+      } else {
+        console.error(`${objectType} fetch issue: ${documents.message}`)
+        break
+      }
+    }
+  }
+
+  console.log(
+    `Fetched ${objects.length} ${
+      objects.length === 1 ? 'object' : 'objects'
+    } for object type: ${objectType}`
+  )
+  console.timeEnd(timeLabel)
 
   // Map and clean data.
-  if (documents.data.objects) {
-    return documents.data.objects.map(item => clean(item))
-  } else {
-    return []
+  if (objects.length > 0) {
+    objects = objects.map(item => clean(item))
   }
+
+  return objects
 }
 
 /**
