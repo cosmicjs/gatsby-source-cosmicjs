4db6630ec9f8d84e3b9428e33bd27a98d12be0bb
Dustin Schau <dustinschau@gmail.com>
2019-09-05 09:43:11 -0700

    chore: add tests

--- src/__tests__/fetch.js
+++ src/__tests__/fetch.js
@@ -2,22 +2,28 @@ jest.mock('axios')
 const axios = require('axios')
 const fetch = require('../fetch')
 
-const getArgs = (additional = {}) => Object.assign({
-  apiURL: `https://api.cosmicjs.com`,
-  bucketSlug: `hello-world`,
-  objectType: `posts`,
-}, additional)
+const getArgs = (additional = {}) =>
+  Object.assign(
+    {
+      apiURL: `https://api.cosmicjs.com`,
+      bucketSlug: `hello-world`,
+      objectType: `posts`,
+    },
+    additional
+  )
 
 test('it passes read_key, if defined', async () => {
   axios.mockResolvedValueOnce({
     data: {
-      objects: []
-    }
+      objects: [],
+    },
   })
 
   const read_key = `hunter2`
 
-  await fetch(getArgs({ apiAccess: { read_key }}))
+  await fetch(getArgs({ apiAccess: { read_key } }))
 
-  expect(axios).toHaveBeenCalledWith(expect.stringContaining(`read_key=hunter2`))
+  expect(axios).toHaveBeenCalledWith(
+    expect.stringContaining(`read_key=hunter2`)
+  )
 })
